65b90ea7b3bf5d2f6dbdd95c9de5b97770c89d97
InternetDominator <sergejs1606@gmail.com>
2025-09-17 08:17:15 +0300

    init

--- src/worker.py
+++
@@ -1,8 +0,0 @@
-from workers import WorkerEntrypoint, Request, Response
-
-class Default(WorkerEntrypoint):
-    async def fetch(self, request: Request):
-        # Super cheap handler to avoid startup CPU
-        if request.url.pathname == "/":
-            return Response.json({"hello": "world"})
-        return Response("Not found", status=404)
